d6c04e79f226b8ac937b230caacbdc3b5458f2a2
Will Storey <will@summercat.com>
2018-02-17 17:44:17 -0800

    Bump version to 1.2

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package main
 
 // CreatedDate is the date we're built. This would be nice to generate
 // dynamically, but I don't want to complicate the build.
-const CreatedDate = "2017-12-17"
+const CreatedDate = "2018-02-17"
 
 // Version is our version.
-const Version = "catbox-1.1"
+const Version = "catbox-1.2"
